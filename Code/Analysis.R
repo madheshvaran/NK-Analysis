8290e908926cb597f7af618e675e728a5b837006
Madhesh <madheshvaran357@gmail.com>
2020-07-22 20:39:18 +0530

    Modified R code to include cell states

--- Code/Analysis.R
+++ Code/Analysis.R
@@ -1,18 +1,19 @@
 library(dplyr)
-library(reshape2)
-library(readr)
 
 receptor <- read.table("Raw Data/Correlation IL2RB Mature NK.txt", header = TRUE, sep = "\t")
 #Run this code for all other cytokine receptor files
 str(receptor)
 colnames(receptor)[colnames(receptor) == "target.gene"] <- "Gene"
 
-list <- read.csv("Raw Data/Genes_correlated_with_sepsis.csv", header = FALSE)
-colnames(list) <- "Gene"
+list <- read.csv("Raw Data/Genes_correlated_with_sepsis.csv")
+str(list)
 
 result <- filter(receptor, Gene %in% list$Gene)
 result$entrez <- NULL
 result$target.probe <- NULL
 result$source.probe <- NULL
+result <- merge(result, list)
+
+result <- result[with(result, order(Score)), ]
 
 write.csv(result, file = "Results/IL2RB Result.csv")
